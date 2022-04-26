439a626d72a4443fceacf152feb77bd50462d537
Guilherme <guilhermeevencioluz@gmail.com>
2022-04-26 18:21:01 -0300

    feat: Requisitos 1 ao 6
    Implementa as funções que validam os inputs de email e senha, juntamente com seus respectivos testes.

---
+++ src/pages/Login/Index.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const emailValidation = () => {
+      const six = 6;
+      const regex = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/i.test(email);
+      if (email === '' || password === '' || password.length <= six || !regex) {
+        setIsDisabled(true);
+      } else {
+        setIsDisabled(false);
+      }
+    };
+
+    emailValidation();
+  }, [email, password]);
+
+  const handleChange = (event) => {
+    switch (event.target.name) {
+    case 'email':
+      setEmail(event.target.value);
+      break;
+    case 'password':
+      setPassword(event.target.value);
+      break;
+    default:
+      break;
+    }
+  };
+
+  const handleEnter = () => {
+    const userObj = { email };
+    window.localStorage.setItem('mealsToken', '1');
+    window.localStorage.setItem('cocktailsToken', '1');
+    window.localStorage.setItem('user', JSON.stringify(userObj));
+    history.push('./');
+  };
+
+  return (
+    <div>
+      <h1>Recipes App</h1>
+      <input
+        data-testid="email-input"
+        type="email"
+        value={ email }
+        name="email"
+        onChange={ handleChange }
+      />
+
+      <input
+        data-testid="password-input"
+        type="password"
+        value={ password }
+        name="password"
+        onChange={ handleChange }
+      />
+
+      <button
+        data-testid="login-submit-btn"
+        type="button"
+        disabled={ isDisabled }
+        onClick={ handleEnter }
+      >
+        Enter
+      </button>
+    </div>
+  );
+}
+
+export default Login;
