a3133fcc61f36fe8afdfcb541be6db9bcee36795
Matheus Pereira <matheusdimmy@hotmail.com>
2022-04-27 13:39:07 -0300

    fix: conserta o redirecionamento no botão que leva pra pagina de comidas

--- src/pages/ExploreFoodsNationalities/index.jsx
+++ src/pages/ExploreFoodsNationalities/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 const ExploreFoodsByNationalities = () => {
   console.log('teste');
   return (
     <div>
       <Header pageName="Explore Nationalities" searchEnabled />
+      <Footer pageName="Explore Nationalities" />
     </div>
   );
 };
