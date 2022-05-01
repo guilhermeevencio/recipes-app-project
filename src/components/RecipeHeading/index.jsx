c85be340876b82b413f91602b0df516536b237ae
Guilherme <guilhermeevencioluz@gmail.com>
2022-05-01 15:07:09 -0300

    feat: botão de favorito
    IMplementa a função que renderiza o botão de favorito de acordo com o seu status no local storage.

--- src/components/RecipeHeading/index.jsx
+++ src/components/RecipeHeading/index.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
-import whiteHeart from '../../images/whiteHeartIcon.svg';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
 import shareIcon from '../../images/shareIcon.svg';
-import blackHeart from '../../images/blackHeartIcon.svg';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
 import AppContext from '../../context/MyContext';
 
 const RecipeHeading = () => {
@@ -10,22 +10,41 @@ const RecipeHeading = () => {
     recipeStatusInfo: {
       isFavorite,
     } } = useContext(AppContext);
+
+  // const [favoriteRecipes] = useLocalStorage('favoriteRecipes', []);
+
+  // useEffect(() => {
+  //   if (recipeDetails) {
+  //     const filteredFavorites = favoriteRecipes
+  //       .filter(({ id }) => id !== recipeDetails.id);
+  //   }
+  // }, []);
+
   return (
     <div style={ { border: '1px solid red' } }>
-      <img
-        src={ recipeDetails.strThumb }
-        alt="Recipe Thumb"
-        data-testid="recipe-photo"
-        width="100%"
-      />
-      <h2 data-testid="recipe-title">{ recipeDetails.str }</h2>
-      <button type="button" data-testid="share-btn">
-        <img src={ shareIcon } alt="share button" />
-      </button>
-      <button type="button" data-testid="favorite-btn">
-        <img src={ isFavorite ? blackHeart : whiteHeart } alt="favorite button" />
-      </button>
-      <p data-testid="recipe-category">{ recipeDetails.category }</p>
+      {recipeDetails
+      && (
+        <>
+          <img
+            src={ recipeDetails.strThumb }
+            alt="Recipe Thumb"
+            data-testid="recipe-photo"
+            width="100%"
+          />
+          <h2 data-testid="recipe-title">{recipeDetails.str}</h2>
+          <button type="button" data-testid="share-btn">
+            <img src={ shareIcon } alt="share button" />
+          </button>
+          <button type="button">
+            <img
+              src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
+              alt="favorite button"
+              data-testid="favorite-btn"
+            />
+          </button>
+          <p data-testid="recipe-category">{recipeDetails.category}</p>
+        </>
+      )}
     </div>
   );
 };
