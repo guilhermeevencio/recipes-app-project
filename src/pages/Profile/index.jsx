487529f7ac03b54f27369bc54857152ef7b7a866
Matheus Pereira <matheusdimmy@hotmail.com>
2022-05-02 15:54:05 -0300

    feat: email do é usuário exibido na tela

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,12 +3,12 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 const Profile = () => {
-  console.log('teste.');
+  const userEmail = localStorage.getItem('user');
   return (
     <div>
       <Header pageName="Profile" searchEnabled={ false } />
       <Footer pageName="Profile" />
-      <div type="text" data-testid="profile-email">email</div>
+      <div type="text" data-testid="profile-email">{userEmail}</div>
       <button type="button" data-testid="profile-done-btn">Done Recipes</button>
       <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
       <button type="button" data-testid="profile-logout-btn">Logout</button>
