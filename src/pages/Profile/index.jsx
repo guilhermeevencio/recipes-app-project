5536802a2971eddaac5e04cbae9f637593abd533
Matheus Pereira <matheusdimmy@hotmail.com>
2022-05-02 16:27:54 -0300

    feat: botões redirecionam para as paginas Done Recipes e Favorite Recipes

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -9,10 +10,29 @@ const Profile = () => {
     <div>
       <Header pageName="Profile" searchEnabled={ false } />
       <Footer pageName="Profile" />
-      <div type="text" data-testid="profile-email">{userEmail}</div>
-      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      <button type="button" data-testid="profile-logout-btn">Logout</button>
+      <div type="text" data-testid="profile-email">{ userEmail }</div>
+      <Link to="/done-recipes">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
     </div>
   );
 };
