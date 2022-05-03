72532c7f1261cdbaeb4754c349b19ab1abd12ed5
Guilherme <guilhermeevencioluz@gmail.com>
2022-05-03 10:45:25 -0300

    feat: custom hooks para salvar no contexto os dados da receita atual.

---
+++ src/customHooks/useFoodRecipeDetails.js
@@ -0,0 +1,43 @@
+import { useContext, useEffect } from 'react';
+import fetchWithId from '../services/fetchWithId';
+import handleWithObjectKeys from '../helpers/RecipeDetailsHelpers';
+import AppContext from '../context/MyContext';
+
+const useFoodRecipeDetails = () => {
+  const { setRecipeDetails } = useContext(AppContext);
+
+  useEffect(() => {
+    const receivedDataWithItemId = async (id) => {
+      const { meals } = await fetchWithId(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+
+      // codigo utilizado para dividir os arrays => https://masteringjs.io/tutorials/fundamentals/filter-key
+
+      const ingredientsObj = Object
+        .fromEntries(Object.entries(meals[0])
+          .filter(([key]) => key.includes('Ingredient')));
+      const ingredientsArr = Object.values(ingredientsObj);
+
+      const measureObj = Object
+        .fromEntries(Object.entries(meals[0])
+          .filter(([key]) => key.includes('Measure')));
+      const measureArr = Object.values(measureObj);
+
+      const newLink = meals[0].strYoutube.replace('https://www.youtube.com/watch?v=',
+        'http://www.youtube.com/embed/');
+
+      const recipeObj = {
+        ...meals[0],
+        ingredients: ingredientsArr,
+        measure: measureArr,
+        type: 'food',
+        videoStr: newLink,
+
+      };
+      const refactoredRecipeObj = handleWithObjectKeys(recipeObj);
+      setRecipeDetails(refactoredRecipeObj);
+    };
+    receivedDataWithItemId();
+  }, [foodId, setRecipeDetails]);
+};
+
+export default useFoodRecipeDetails;
