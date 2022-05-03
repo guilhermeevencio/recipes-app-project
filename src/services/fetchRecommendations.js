02900260a5b9765b2ef6ff0ae32b5f8b921947d6
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-05-03 11:31:48 -0300

    fix: Raname a string into a fetchRecommendations

--- src/services/fetchRecommendations.js
+++ src/services/fetchRecommendations.js
@@ -4,8 +4,8 @@ const fetchRecomendations = async (url, pageName) => {
   const recipes = await fetchFromApi(url);
   const numberOfRecomendations = 6;
   if (pageName === 'Foods') {
-    const teste = [...recipes.drinks];
-    const sixRecomentations = teste.splice(0, numberOfRecomendations);
+    const newRecipes = [...recipes.drinks];
+    const sixRecomentations = newRecipes.splice(0, numberOfRecomendations);
     const drinksResult = sixRecomentations
       .map(({ idDrink, strDrinkThumb, strDrink }) => (
         { id: idDrink, strThumb: strDrinkThumb, str: strDrink, page: 'drinks' }
@@ -13,8 +13,8 @@ const fetchRecomendations = async (url, pageName) => {
     return drinksResult;
   }
   if (pageName === 'Drinks') {
-    const teste = [...recipes.meals];
-    const sixRecomentations = teste.splice(0, numberOfRecomendations);
+    const newRecipes = [...recipes.meals];
+    const sixRecomentations = newRecipes.splice(0, numberOfRecomendations);
     const mealsResult = sixRecomentations
       .map(({ idMeal, strMealThumb, strMeal }) => (
         { id: idMeal, strThumb: strMealThumb, str: strMeal, page: 'foods' }
