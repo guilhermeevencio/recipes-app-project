8aa36f939396c23aeb99b9bca4b9f0662cee15a7
Matheus Pereira <matheusdimmy@hotmail.com>
2022-05-03 00:07:33 -0300

    feat: tela de explorar comidas e bebidas quase finalizada, requisitos 70 a 71 OK

--- src/pages/ExploreDrinks/index.jsx
+++ src/pages/ExploreDrinks/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -8,6 +9,12 @@ const ExploreDrinks = () => {
     <div>
       <Header pageName="Explore Drinks" searchEnabled={ false } />
       <Footer pageName="Explore Drinks" />
+      <Link to="/explore/drinks/ingredients">
+        <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
+      </Link>
+      <Link to="/">
+        <button type="button" data-testid="explore-surprise">Surprise me!</button>
+      </Link>
     </div>
   );
 };
--- src/pages/ExploreFoods/index.jsx
+++ src/pages/ExploreFoods/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -8,6 +9,15 @@ const ExploreFoods = () => {
     <div>
       <Header pageName="Explore Foods" searchEnabled={ false } />
       <Footer pageName="Explore Foods" />
+      <Link to="/explore/foods/ingredients">
+        <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
+      </Link>
+      <Link to="/explore/foods/nationalities">
+        <button type="button" data-testid="explore-by-nationality">By Nationality</button>
+      </Link>
+      <Link to="/">
+        <button type="button" data-testid="explore-surprise">Surprise me!</button>
+      </Link>
     </div>
   );
 };
