39dbbb5c8b690ad37c5ca1bd170e787b4732a73c
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-05-04 19:09:07 -0300

    feat(FavoriteRecipes): req 60 ao 62 completo e passando no teste
    Os requisitos 60 ao 62 foram feitos nesse commit e parte do 63

--- src/pages/FavoriteRecipes/index.jsx
+++ src/pages/FavoriteRecipes/index.jsx
@@ -1,11 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
+import shareIcon from '../../images/shareIcon.svg';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
+
+const THREE_SECOND = 3000;
 
 const FavoriteRecipes = () => {
-  console.log('teste');
+  const [favorites, setFavorites] = useState([]);
+  const [isLinkCopied, setIsLinkCopied] = useState(false);
+
+  useEffect(() => {
+    const result = localStorage.getItem('favoriteRecipes');
+    const favoritesFromLocalStorage = JSON.parse(result);
+    setFavorites(favoritesFromLocalStorage);
+  }, []);
+
+  // useEffect de CONSOLE LOG
+  // useEffect(() => {
+  //   console.log(props);
+  // }, [favorites]);
+
+  const handleShareBtn = () => {
+    setIsLinkCopied(true);
+    setTimeout(() => setIsLinkCopied(false), THREE_SECOND);
+    navigator.clipboard.writeText('Xlabauuuuuu!');
+  };
+
   return (
     <div>
       <Header pageName="Favorite Recipes" searchEnabled={ false } />
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drink
+      </button>
+
+      {favorites && favorites.map((
+        { id, image, name, nationality, category, type, alcoholicOrNot }, index,
+      ) => (
+        <div key={ id }>
+          <img
+            data-testid={ `${index}-horizontal-image` }
+            src={ image }
+            alt={ name }
+            width={ 90 }
+          />
+
+          <div>
+            <div data-testid={ `${index}-horizontal-top-text` }>
+              { type === 'food' ? `${nationality} - ${category}` : alcoholicOrNot}
+            </div>
+            <div data-testid={ `${index}-horizontal-name` }>{name}</div>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              onClick={ handleShareBtn }
+            >
+              <img src={ shareIcon } alt="Botão de compartilhar" />
+            </button>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              src={ blackHeartIcon }
+            >
+              <img
+                src={ blackHeartIcon }
+                alt="Botão de favoritar"
+              />
+            </button>
+            {isLinkCopied && <div>Link copied!</div>}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
