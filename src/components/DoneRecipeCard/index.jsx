3e43c0fdb5b7e7c15007a7422585404fc122170a
Guilherme <guilhermeevencioluz@gmail.com>
2022-05-07 15:18:59 -0300

    feat: adiciona a rota ao clicar nas imagens da pagina de recipesDone.

--- src/components/DoneRecipeCard/index.jsx
+++ src/components/DoneRecipeCard/index.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import shareImg from '../../images/shareIcon.svg';
 
 const DoneRecipeCard = (props) => {
   const { recipe, index } = props;
+  const [isLinkCopied, setIsLinkCopied] = useState(false);
   return (
     <div>
-      <img
-        src={ recipe.image }
-        alt="Recipe Item"
-        data-testid={ `${index}-horizontal-image` }
-      />
+      <Link to={ `/${recipe.type}s/${recipe.id}` }>
+        <img
+          src={ recipe.image }
+          alt="Recipe Item"
+          data-testid={ `${index}-horizontal-image` }
+        />
+      </Link>
       <div>
         <p
           data-testid={ `${index}-horizontal-top-text` }
@@ -19,11 +23,13 @@ const DoneRecipeCard = (props) => {
             ? `${recipe.nationality} - ${recipe.category}`
             : `${recipe.alcoholicOrNot}`}
         </p>
-        <h4
-          data-testid={ `${index}-horizontal-name` }
-        >
-          {recipe.name}
-        </h4>
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <h4
+            data-testid={ `${index}-horizontal-name` }
+          >
+            {recipe.name}
+          </h4>
+        </Link>
         <p
           data-testid={ `${index}-horizontal-done-date` }
         >
@@ -31,6 +37,10 @@ const DoneRecipeCard = (props) => {
         </p>
         <button
           type="button"
+          onClick={ () => {
+            navigator.clipboard.writeText(`http://localhost:3000/${recipe.type}s/${recipe.id}`);
+            setIsLinkCopied(true);
+          } }
         >
           <img
             src={ shareImg }
@@ -38,6 +48,7 @@ const DoneRecipeCard = (props) => {
             data-testid={ `${index}-horizontal-share-btn` }
           />
         </button>
+        {isLinkCopied && <p>Link copied!</p>}
         {recipe.type === 'food'
         && recipe.tags.filter((_e, i) => i < 2).map((tag) => (
           <div key={ tag }>
--- src/pages/DoneRecipes/index.jsx
+++ src/pages/DoneRecipes/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DoneRecipeCard from '../../components/DoneRecipeCard';
 import Header from '../../components/Header';
 import useLocalStorage from '../../customHooks/useLocalStorage';
 
 const DoneRecipes = () => {
   const [doneRecipes] = useLocalStorage('doneRecipes', []);
-  console.log(doneRecipes);
+  const [recipeType, setRecipeType] = useState('all');
+  const [recipes, setRecipes] = useState(doneRecipes);
+
+  useEffect(() => {
+    switch (recipeType) {
+    case 'food':
+      setRecipes(doneRecipes.filter(({ type }) => type === 'food'));
+      break;
+    case 'drink':
+      setRecipes(doneRecipes.filter(({ type }) => type === 'drink'));
+      break;
+    default:
+      setRecipes(doneRecipes);
+      break;
+    }
+  }, [doneRecipes, recipeType]);
   return (
     <div>
       <Header pageName="Done Recipes" searchEnabled={ false } />
@@ -13,23 +28,26 @@ const DoneRecipes = () => {
         <button
           type="button"
           data-testid="filter-by-all-btn"
+          onClick={ () => setRecipeType('all') }
         >
           All
         </button>
         <button
           type="button"
           data-testid="filter-by-food-btn"
+          onClick={ () => setRecipeType('food') }
         >
           Food
         </button>
         <button
           type="button"
           data-testid="filter-by-drink-btn"
+          onClick={ () => setRecipeType('drink') }
         >
           Drink
         </button>
       </div>
-      {doneRecipes.map((recipe, index) => (
+      {recipes.map((recipe, index) => (
         <DoneRecipeCard key={ recipe.id } recipe={ recipe } index={ index } />
       ))}
     </div>
